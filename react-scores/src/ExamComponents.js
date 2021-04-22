b17f76d5829fcf6c42b2bb4f4d7940e3fba9e7ad
Fulvio Corno <fulvio.corno@polito.it>
2021-04-22 10:11:48 +0200

    implement the Form to add a new exam

--- react-scores/src/ExamComponents.js
+++ react-scores/src/ExamComponents.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { useState } from 'react';
 import { Col, Table } from 'react-bootstrap';
 import { iconDelete, iconEdit } from './icons'
@@ -10,35 +11,43 @@ function Title(props) {
 
 function ExamTable(props) {
 
-    const [exams, setExams] = useState(props.exams) ;
+    const [exams, setExams] = useState(props.exams);
 
     const deleteExam = (code) => {
-        setExams( oldExams => oldExams.filter( exam => exam.coursecode !== code) ) ;
+        setExams(oldExams => oldExams.filter(exam => exam.coursecode !== code));
     }
 
-    return (<Table striped bordered>
-        <thead>
-            <tr>
-                <th>Exam</th>
-                <th>Score</th>
-                <th>Date</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            {exams.map((exam => <ExamRow key={exam.coursecode} exam={exam} 
-            examName={props.courses.filter(c=>c.coursecode === exam.coursecode)[0].name}
-            deleteExam={deleteExam}
-             />))}
-        </tbody>
-    </Table>
+    const addExam = (newExam) => {
+        // exams.push(exam) ; NOOO
+        setExams( oldExams => [...oldExams, newExam]);
+    } ;
+
+    return (<>
+        <Table striped bordered>
+            <thead>
+                <tr>
+                    <th>Exam</th>
+                    <th>Score</th>
+                    <th>Date</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {exams.map((exam => <ExamRow key={exam.coursecode} exam={exam}
+                    examName={props.courses.filter(c => c.coursecode === exam.coursecode)[0].name}
+                    deleteExam={deleteExam}
+                />))}
+            </tbody>
+        </Table>
+        <ExamForm courses={props.courses} addExam={addExam} />
+    </>
     );
 }
 
 function ExamRow(props) {
     return (<tr>
         <ExamInfo {...props} />
-        <ExamControls exam={props.exam} deleteExam={props.deleteExam}/>
+        <ExamControls exam={props.exam} deleteExam={props.deleteExam} />
     </tr>
     );
 }
@@ -52,7 +61,33 @@ function ExamInfo(props) {
 }
 
 function ExamControls(props) {
-    return <td>{iconEdit} <span onClick={()=>props.deleteExam(props.exam.coursecode)}>{iconDelete}</span></td> ;
+    return <td>{iconEdit} <span onClick={() => props.deleteExam(props.exam.coursecode)}>{iconDelete}</span></td>;
+}
+
+function ExamForm(props) {
+    const [course, setCourse] = useState('') ;  // props.courses[0].coursecode
+    const [score, setScore] = useState('') ;
+    const [date, setDate] = useState('') ;
+
+    const handleAdd = (event) => {
+        event.preventDefault() ;
+
+        const exam = { coursecode: course, score: score, date: dayjs(date) } ;
+        // MUST DO VALIDATION!!!!
+        props.addExam(exam);
+
+    } ;
+
+    return (
+        <form>
+        Exam: <select value={course} onChange={ev=>setCourse(ev.target.value)}>
+            <option value='' disabled>Choose one...</option>
+            {props.courses.map(course => <option key={course.coursecode} value={course.coursecode}>{course.name}</option>)}
+            </select><br />
+        Score: <input type='text' value={score} onChange={(event)=>{setScore(event.target.value)}} /><br />
+        Date: <input type='date' value={date} onChange={ev=>setDate(ev.target.value)}/><br />
+            <button onClick={handleAdd}>Add</button>
+        </form>);
 }
 
 export { Title, ExamTable };
